7ecc0e0df9d5afc2ff6f8798a328fdacacb261bb
Lucifer <91655603+LuciferMoringStarDevil@users.noreply.github.com>
2021-11-06 10:33:09 +0530

    Update OMDB.py

--- OMDB.py
+++ OMDB.py
@@ -19,12 +19,15 @@ def get_movie_info(movieTitle):
     movie_info["genre"] = response.get("Genre")
     movie_info["director"] = response.get("Director")
     movie_info["writer"] = response.get("Writer")
+    movie_info["actors"] = response.get("Actors")
     movie_info["plot"] = response.get("Plot")
+    movie_info["language"] = response.get("Language")
     movie_info["country"] = response.get("Country")
     movie_info["ratings"] = response.get("Ratings")
     movie_info["imdb_rating"] = response.get("imdbRating")
     movie_info["votes"] = response.get("imdbVotes")
     movie_info["imdb_id"] = response.get("imdbID")
     movie_info["pimage"] = response.get("Poster")
+    movie_info["box_office"] = response.get("box office")
 
     return movie_info
